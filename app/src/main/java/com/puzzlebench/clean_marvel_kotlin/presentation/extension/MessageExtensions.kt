a473a0292e21745d2bd1ff0b24e8756cbf1f02ba
regmarcos <marcos.reginato@globant.com>
2019-12-11 13:46:59 -0300

    added api keys

--- app/src/main/java/com/puzzlebench/clean_marvel_kotlin/presentation/extension/MessageExtensions.kt
+++ app/src/main/java/com/puzzlebench/clean_marvel_kotlin/presentation/extension/MessageExtensions.kt
@@ -6,7 +6,7 @@ import android.widget.Toast
 
 
 // eg: extension function default parameter
-fun Context.showToast(message: String, duration: Int = Toast.LENGTH_SHORT) = Toast.makeText(this, message, duration).show()
+fun Context.showToast(message: String, duration: Int = Toast.LENGTH_LONG) = Toast.makeText(this, message, duration).show()
 
 // eg: extension function
 fun RecyclerView.ViewHolder.showToast(message: String) = itemView.context.showToast(message)
