00e6c75ffd18b64ed9043e4fe401ddee62c1a759
regmarcos <marcos.reginato@globant.com>
2020-01-02 11:27:38 -0300

    added database_fab

--- app/src/main/java/com/puzzlebench/clean_marvel_kotlin/presentation/MainActivity.kt
+++ app/src/main/java/com/puzzlebench/clean_marvel_kotlin/presentation/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.puzzlebench.clean_marvel_kotlin.presentation
 
-import android.os.Build
 import android.os.Bundle
-import androidx.annotation.RequiresApi
 import com.puzzlebench.clean_marvel_kotlin.R
 import com.puzzlebench.cmk.data.mapper.repository.CharacterMapperRepository
 import com.puzzlebench.cmk.data.repository.CharacterDataRepository
@@ -14,7 +12,7 @@ import com.puzzlebench.clean_marvel_kotlin.presentation.base.BaseRxActivity
 import com.puzzlebench.clean_marvel_kotlin.presentation.mvp.CharacterPresenter
 import com.puzzlebench.clean_marvel_kotlin.presentation.mvp.CharacterView
 import com.puzzlebench.cmk.domain.usecase.SaveCharacterRepositoryUseCase
-import kotlinx.android.synthetic.main.activity_main.floating_action_button
+import kotlinx.android.synthetic.main.activity_main.refresh_fab
 
 class MainActivity : BaseRxActivity() {
 
@@ -32,6 +30,6 @@ class MainActivity : BaseRxActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         presenter.init()
-        floating_action_button.setOnClickListener { presenter.onClickFAB() }
+        refresh_fab.setOnClickListener { presenter.onClickFAB() }
     }
 }
--- app/src/main/java/com/puzzlebench/clean_marvel_kotlin/presentation/mvp/CharacterView.kt
+++ app/src/main/java/com/puzzlebench/clean_marvel_kotlin/presentation/mvp/CharacterView.kt
@@ -9,9 +9,10 @@ import com.puzzlebench.clean_marvel_kotlin.presentation.TAG
 import com.puzzlebench.clean_marvel_kotlin.presentation.adapter.CharacterAdapter
 import com.puzzlebench.clean_marvel_kotlin.presentation.extension.showToast
 import com.puzzlebench.clean_marvel_kotlin.presentation.fragments.CharacterFragmentDialog
-import kotlinx.android.synthetic.main.activity_main.floating_action_button
+import kotlinx.android.synthetic.main.activity_main.database_fab
 import kotlinx.android.synthetic.main.activity_main.progressBar
 import kotlinx.android.synthetic.main.activity_main.recycleView
+import kotlinx.android.synthetic.main.activity_main.refresh_fab
 import java.lang.ref.WeakReference
 
 class CharacterView(activity: MainActivity) {
@@ -52,11 +53,13 @@ class CharacterView(activity: MainActivity) {
     }
 
     fun showFAB() {
-        activityRef.get()?.let { it.floating_action_button.visibility = View.VISIBLE }
+        activityRef.get()?.let { it.refresh_fab.visibility = View.VISIBLE }
+        activityRef.get()?.let { it.database_fab.visibility = View.VISIBLE }
     }
 
     fun hideFAB() {
-        activityRef.get()?.let { it.floating_action_button.visibility = View.GONE }
+        activityRef.get()?.let { it.refresh_fab.visibility = View.GONE }
+        activityRef.get()?.let { it.database_fab.visibility = View.VISIBLE }
     }
 
     private fun showFragmentDialog(character: Character){
